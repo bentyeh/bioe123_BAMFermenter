fe9b4da2abd404248dd7f1dc5db13008efe16e68
Benjamin Yeh <bentyeh@gmail.com>
2018-03-06 15:56:40 -0800

    code to read and plot eeprom data

---
+++ eeprom/eeprom.ino
@@ -0,0 +1,90 @@
+#include <EEPROM.h>
+
+/** the current address in the EEPROM (i.e. which byte we're going to read from next) **/
+int lastaddr;
+
+unsigned int time_min;
+byte heat_set;
+byte stir_set;
+byte air_set;
+byte fan_set;
+unsigned int od;
+unsigned int purple;
+unsigned int temp;
+
+String START_FLAG = "start";
+String END_FLAG = "end";
+
+const int totalsize = sizeof(heat_set) + sizeof(stir_set) + sizeof(air_set) + sizeof(fan_set) + sizeof(od) + sizeof(purple) + sizeof(temp);
+const int int_mask = ( 1 << 8 ) - 1;
+
+void setup() {
+  // initialize serial
+  Serial.begin(9600);
+
+  // wait for serial port to connect. Needed for native USB port only
+  while (!Serial) {
+    ;
+  }
+
+  // write test data to EEPROM
+  for(int i = 1; i < EEPROM.length(); i++) {
+    EEPROM.write(i, i & int_mask);
+    EEPROM.update(0, i);
+  }
+  EEPROM.get(0, lastaddr);
+}
+
+void loop() {
+  // read a byte from the current address of the EEPROM
+  int addr = sizeof(lastaddr);
+  Serial.println(lastaddr);
+
+  // print start flag
+  Serial.println(START_FLAG);
+
+  // print header
+  Serial.print("time ");
+  Serial.print("heat ");
+  Serial.print("stir ");
+  Serial.print("air ");
+  Serial.print("fan ");
+  Serial.print("od ");
+  Serial.print("purple ");
+  Serial.println("temp ");
+
+
+  // loop until reading last value
+  while(addr < lastaddr - totalsize) {
+    // read values
+    EEPROM.get(addr, time_min);
+    addr += sizeof(time_min);
+    EEPROM.get(addr, heat_set);
+    addr += sizeof(heat_set);
+    EEPROM.get(addr, stir_set);
+    addr += sizeof(stir_set);
+    EEPROM.get(addr, air_set);
+    addr += sizeof(air_set);
+    EEPROM.get(addr, fan_set);
+    addr += sizeof(fan_set);
+    EEPROM.get(addr, od);
+    addr += sizeof(od);
+    EEPROM.get(addr, purple);
+    addr += sizeof(purple);
+    EEPROM.get(addr, temp);
+    addr += sizeof(temp);
+
+    // print values
+    Serial.print(time_min); Serial.print(" ");
+    Serial.print(heat_set); Serial.print(" ");
+    Serial.print(stir_set); Serial.print(" ");
+    Serial.print(air_set); Serial.print(" ");
+    Serial.print(fan_set); Serial.print(" ");
+    Serial.print(od); Serial.print(" ");
+    Serial.print(purple); Serial.print(" ");
+    Serial.print(temp); Serial.println(" ");
+  }
+
+  // print end flag
+  Serial.println(END_FLAG);
+}
---
+++ eeprom/eeprom.py
@@ -0,0 +1,94 @@
+"""
+Display analog data from Arduino using Python (matplotlib)
+Code editted by J. Sganga for Bioe123
+Date: 2/20/2017
+Original:
+Author: Mahesh Venkitachalam
+Website: electronut.in
+git: https://gist.github.com/electronut/d5e5f68c610821e311b0
+"""
+
+import sys, serial
+import numpy as np
+# import pandas as pd
+from collections import deque
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+
+
+def main(port, baud_rate):
+
+    START_FLAG = "start"
+    END_FLAG = "end"
+
+    arduino = serial.Serial(port, baud_rate)
+
+    print("Serial connected")
+
+    # wait for START_FLAG
+    while True:
+        # wait for serial output
+        while arduino.inWaiting == 0:
+            print("here")
+            pass
+        stream = arduino.readline().decode("utf-8").rstrip()
+        if stream == START_FLAG:
+            print("start received")
+            break
+
+    # get header
+    while arduino.inWaiting == 0:
+        pass
+    stream = arduino.readline().decode("utf-8").rstrip()
+    header = stream.split()
+    print("Header received: ")
+    print(header)
+
+    # initialize data list
+    data = []
+
+    # read until END_FLAG
+    while True:
+        while arduino.inWaiting == 0:
+            pass
+        stream = arduino.readline().decode("utf-8").rstrip()
+        if stream == END_FLAG:
+            print("end received")
+            break
+
+        # time, temp, od, purple, stir, air, heat, fan = [int(val) for val in stream.split()]
+        data.append([int(val) for val in stream.split()])
+
+    # convert data to numpy array
+    print(data)
+    data = np.array(data)
+    print(data)
+    time = data[:,0]
+    num_vars = data.shape[1] - 1
+
+    # plot data
+    f, axarr = plt.subplots(num_vars, sharex=True)
+    f.suptitle('Recorded data')
+    for i in range(data.shape[1] - 1):
+        axarr[i].plot(time,data[:,i+1])
+        axarr[i].set_ylabel(header[i+1])
+
+    axarr[-1].set_xlabel(header[0])
+
+    plt.savefig("eeprom.png")
+    plt.show()
+    
+"""
+call main, CHANGE THESE VALUES!!!
+replace 'COM6' with whatever the tools pulldown bar shows you're connected to
+for mac, it looks like '/dev/cu.usbmodem....' (without the (Arduino/Genuino Micro))
+Also, note the arduino puts out data faster than we can plot in real time, so there's 
+a delay between the plotted value and the actual value (~1-2 min)
+Format for data in Arduino Serial print, data values separated by space, new line for each time point:
+Serial.print(data_value_1);
+Serial.print(' ');
+...
+Serial.println(data_value_n);
+"""
+if __name__ == '__main__':
+    main('COM3', 9600)
